ff9a9bf5fb6ac34f5f2e0e0e8ec37bc935f54440
Axel Breens <axel@abrconsultancy.be>
2021-04-11 17:48:27 +0200

    Exercises of Chapter 3

--- hello.py
+++ hello.py
@@ -1,26 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-        return '<h1>Welcome Welcome Welcome</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-        return '<h1>Hello, {}!<h1>'.format(name)
-
-
-@app.route('/about')
-def about_me():
-        return '<h1>About About About</h1>'
-
-
-@app.route('/portfolio')
-def portfolio():
-        return '<h1>Portfolio Portfolio Portfolio</h1>'
-
-
-if __name__ == '__main__':
-        app.run()
+    return render_template('user.html', name=name)
